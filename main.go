8e5bc30f3db7458a7de8f0968595e6f8b1c2220e
Omar Saad <omar.saad0705@gmail.com>
2022-01-11 19:49:19 +0100

    testValidity estimate

--- main.go
+++ main.go
@@ -5,3 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+/*  Test the validity of a string in the spec
+ *
+ *	returns boolean flag true if the given string complies with the format, or false if the string does not comply
+ *
+ *	Estimated time: 20min
+ *  Used time: ...
+ */
+func testValidity(str string) bool {
+
+}
